f2e76eb793ed89436c4be647027fadcd0fd8ad11
kevin <kn880701@gmail.com>
2026-07-06 11:17:19 +0800

    [fix] 修正重新整理404問題

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+const redirectPath = window.sessionStorage.getItem("gh-pages-redirect-path");
+if (redirectPath) {
+    window.sessionStorage.removeItem("gh-pages-redirect-path");
+    window.history.replaceState(null, "", redirectPath);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
